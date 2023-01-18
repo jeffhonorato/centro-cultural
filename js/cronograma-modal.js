c5f665e752a43ddde550ef43b8c7064d1a6f7db7
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-01-18 10:07:31 -0300

    Ajustes

--- js/cronograma-modal.js
+++ js/cronograma-modal.js
@@ -5,17 +5,19 @@ const cardMesJaneiro = () => {
         const modalMes = document.querySelector(".js-modal-janeiro");
         modalMes.classList.add("ativo"); 
         e.stopPropagation();
+        const barraDeRolagem = document.querySelector(".js-scroll");
+            barraDeRolagem.classList.add("off")
     });
 
     const btnFechar = document.querySelector(".js-btnFechar-modal-janeiro");
         btnFechar.addEventListener("click", () => {
             const modalMes = document.querySelector(".js-modal-janeiro");
             modalMes.classList.remove("ativo");  
+            const barraDeRolagem = document.querySelector(".js-scroll");
+            barraDeRolagem.classList.remove("off")
         });
-    
-    
+
+        
 }
-
-
 
 cardMesJaneiro();

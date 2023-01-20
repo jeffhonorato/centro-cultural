3e75fd2c8b605b0d01102c5ec953564f6d279a29
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-01-20 10:06:13 -0300

    Ajustes

--- js/projetos.js
+++ js/projetos.js
@@ -1,5 +1,12 @@
-const btnMusica = document.querySelector(".js-btn-musica");
+const projetoMusica = () => {
+    const btnMusica = document.querySelector(".js-btn-musica");
     btnMusica.addEventListener("click", () => {
         const modalMusica = document.querySelector(".js-projeto-modal-musica")
         modalMusica.classList.add("ativo");
+        console.log(modalMusica)
     });
+   
+}
+
+
+projetoMusica();    

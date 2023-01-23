41b118b117fb20d038969ee7d3689c0c72e10df3
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-01-23 14:38:43 -0300

    Ajustes

--- js/projetos/projeto-musica.js
+++ js/projetos/projeto-musica.js
@@ -19,3 +19,25 @@ var swiper = new Swiper(".galeria-musica", {
         prevEl: ".swiper-button-prev",
       },
   });
+
+  var swiper = new Swiper(".tocandoViolao", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: "true",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });

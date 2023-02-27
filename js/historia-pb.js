6cefc8d59510a2e10295d4d6ff8fd15b4230a8df
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-02-27 09:30:48 -0300

    Ajustes

--- js/historia-pb.js
+++ js/historia-pb.js
@@ -75,3 +75,22 @@ var swiper = new Swiper('.historiaPB', {
         prevEl: ".swiper-button-prev",
     },
   });
+
+  var swiper = new Swiper('.caatinga', {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 1,
+      stretch: 0,
+      depth: 100,
+      modifier: 9,
+      slideShadows: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
